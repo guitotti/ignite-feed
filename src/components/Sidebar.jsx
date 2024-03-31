a8984c8ab35be69e215085a239b10afe822949ff
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-03-30 22:44:12 -0300

    ✨ feature: create Sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import { Pencil } from 'phosphor-react'
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+  return(
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=80&w=2106&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+    
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/guitotti.png" alt="" />
+
+        <strong>Guilherme Totti</strong>
+        <span>Web Developer</span>
+      </div> 
+
+      <footer>
+        <a href="#">
+          <Pencil size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
