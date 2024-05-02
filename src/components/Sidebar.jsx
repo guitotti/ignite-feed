8a321c22bb2b32c1281d6616a63ed59e2e0414df
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-05-02 18:28:10 -0300

    ✨ feat: create avatar component and use it in sidebar, post and comment components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,25 +1,30 @@
-import { Pencil } from 'phosphor-react'
+import { Pencil } from "phosphor-react";
 
-import styles from './Sidebar.module.css'
+import styles from "./Sidebar.module.css";
+import { Avatar } from "./Avatar";
 
 export function Sidebar() {
-  return(
+  return (
     <aside className={styles.sidebar}>
-      <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=80&w=2106&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-    
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=80&w=2106&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
+
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/guitotti.png" alt="" />
+        <Avatar src="https://github.com/guitotti.png" />
 
         <strong>Guilherme Totti</strong>
         <span>Web Developer</span>
-      </div> 
+      </div>
 
       <footer>
         <a href="#">
-          <Pencil size={20}/>
+          <Pencil size={20} />
           Editar seu perfil
         </a>
       </footer>
     </aside>
-  )
+  );
 }
